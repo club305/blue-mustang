73ad371b0d7ce69f605f6a0582cbdbd315fd8686
bluemustangs <marcus.coates12@gmail.com>
2018-04-23 21:38:03 -0400

    Initial commit.

---
+++ blue-mustang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blue-mustang
+//
+//  Created by Marcus Coates on 4/23/18.
+//  Copyright © 2018 Marcus L. Coates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
